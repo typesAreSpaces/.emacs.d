52b884b5feaedce0706b96e545e62204cc19b976
jose <jabelcastellanosjoo@unm.edu>
2022-12-17 14:49:55 -0700

    Add update branches script.

---
+++ update_branches.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+brach=$(git rev-parse --abbrev-ref HEAD)
+git checkout main
+git pull
+git checkout cs-unm-machine
+git pull
+git merge main -m 'Update cs-unm-machine branch'
+git push
+git checkout usb-stick
+git pull
+git merge main -m 'Update usb-stick branch'
+git push
+git checkout no-evil-layer
+git pull
+git merge main -m 'Update no-evil-layer branch'
+git push
+git checkout macos
+git pull
+git merge main -m 'Update macos branch'
+git push
+git checkout no-evil-layer-macos
+git pull
+git merge macos -m 'Update no-evil-layer-macos branch'
+git push
+git checkout $branch
