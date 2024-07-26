47f9ffead92fd90970056012b64b3de193ba2e97
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2024-07-26 15:08:22 -0600

    Minor changes.

--- init.el
+++ init.el
@@ -448,7 +448,8 @@
   :diminish which-key-mode
   :config
   (which-key-mode)
-  (setq which-key-idle-delay 1))
+  (setq which-key-idle-delay 1)
+  (setq which-key-idle-secondary-delay 0.05))
 
 (use-package flx)
 
